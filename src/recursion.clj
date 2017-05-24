aa7e6ad3fbfc7713189b2ad86a861661df86f06a
anttikar <antti.karhu@solita.fi>
2017-05-24 17:31:55 +0300

    .

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,37 +1,92 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (*
+      (first coll)
+      (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (if (empty? coll)
+    false
+    (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (if (empty? coll)
+    nil
+    (if (singleton? coll)
+      (first coll)
+      (my-last (rest coll)))))
 
 (defn max-element [a-seq]
-  :-)
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (max-element
+        (assoc
+          (vec (rest a-seq)) 0
+            (max (first a-seq) (second a-seq)))))))
+
+(defn max-element2 [a-seq]
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (max-element
+        (cons (max (first a-seq) (second a-seq)) (rest (rest a-seq)))))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (> (count seq-1) (count seq-2))
+    seq-1
+    seq-2))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    nil
+    (if (singleton? a-seq)
+      (first a-seq)
+      (longest-sequence
+        (assoc
+          (vec (rest a-seq)) 0
+            (seq-max (first a-seq) (second a-seq)))))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons
+        (first a-seq)
+        (my-filter pred? (rest a-seq)))
+      (my-filter pred? (rest a-seq)))))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (cond
+    (empty? a-seq) false
+    (= elem (first a-seq)) true
+    :else (sequence-contains? elem (rest a-seq))))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    (pred? (first a-seq)) (cons (first a-seq) (my-take-while pred? (rest a-seq)))
+    :else []))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) a-seq
+    (pred? (first a-seq)) (my-drop-while pred? (rest a-seq))
+    :else a-seq))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (if (not (= (count a-seq) (count b-seq)))
+    false
+    (if (empty? a-seq)
+      true
+      (if (= (first a-seq) (first b-seq))
+        (seq= (rest a-seq) (rest b-seq))
+        false))))
 
 (defn my-map [f seq-1 seq-2]
   [:-])
@@ -89,4 +144,3 @@
 
 (defn powerset [a-set]
   [:-])
-
